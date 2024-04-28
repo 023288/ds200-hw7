86eae173f7c40440d58a078abd3bd2f7f350a243
023288 <115693115+023288@users.noreply.github.com>
2024-04-28 16:10:33 -0700

    split data and specify columns

--- hw7.py
+++ hw7.py
@@ -1,10 +1,17 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn.model_selection
 
 # Load the datasets
 path = '~/Spaceship titanic/ds200-hw7/'
-train_data = pd.read_csv(path + 'train.csv')
-test_data = pd.read_csv(path + 'test.csv')
+df_labeled = pd.read_csv(path + 'train.csv')
+df_test = pd.read_csv(path + 'test.csv')
 sample_submission = pd.read_csv(path + 'sample_submission.csv')
 
-# Display the first few rows of the training data and summarize missing values
-train_data.head(), train_data.isnull().sum(), train_data.info(), test_data.head(), test_data.isnull().sum(), test_data.info()
+# Split into train and validation sets
+df_train, df_val = sklearn.model_selection.train_test_split(df_labeled, test_size=0.2, random_state=42)
+
+# Specify columns
+numerical_cols = ['Age', 'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
+categorical_cols = ['HomePlanet', 'CryoSleep', 'Cabin', 'Destination', 'VIP']
