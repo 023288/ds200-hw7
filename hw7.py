90ddbd8d3c737673ac57f5306a6134b5b7c9360c
023288 <115693115+023288@users.noreply.github.com>
2024-04-28 16:30:34 -0700

    handle missing data values

--- hw7.py
+++ hw7.py
@@ -1,7 +1,10 @@
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import sklearn.model_selection
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 
 # Load the datasets
 path = '~/Spaceship titanic/ds200-hw7/'
@@ -15,3 +18,16 @@ df_train, df_val = sklearn.model_selection.train_test_split(df_labeled, test_siz
 # Specify columns
 numerical_cols = ['Age', 'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
 categorical_cols = ['HomePlanet', 'CryoSleep', 'Cabin', 'Destination', 'VIP']
+
+# Impute missing values
+impute_vals = {
+    'Age': df_train['Age'].median(),
+    'RoomService': df_train['RoomService'].median(),
+    'FoodCourt': df_train['FoodCourt'].median(),
+    'ShoppingMall': df_train['ShoppingMall'].median(),
+    'Spa': df_train['Spa'].median(),
+    'VRDeck': df_train['VRDeck'].median()
+}
+
+df_train.fillna(impute_vals, inplace=True)
+df_val.fillna(impute_vals, inplace=True)
